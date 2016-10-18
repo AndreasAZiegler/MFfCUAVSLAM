822c9f1a94ceaee4ffffce24b4e47c509e2509ec
Andreas Ziegler <062.127@gmail.com>
2016-10-18 20:57:15 +0200

    SOFT: Reinitiated macslam initial commit.

---
+++ catkin_ws/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/catkin/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/DBoW2/BowVector.cpp" "/data/catkin/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DBoW2/BowVector.cpp.o"
+  "/data/catkin/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/DBoW2/FORB.cpp" "/data/catkin/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DBoW2/FORB.cpp.o"
+  "/data/catkin/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/DBoW2/FeatureVector.cpp" "/data/catkin/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DBoW2/FeatureVector.cpp.o"
+  "/data/catkin/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/DBoW2/ScoringObject.cpp" "/data/catkin/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DBoW2/ScoringObject.cpp.o"
+  "/data/catkin/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/DUtils/Random.cpp" "/data/catkin/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DUtils/Random.cpp.o"
+  "/data/catkin/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/DUtils/Timestamp.cpp" "/data/catkin/src/v4rl_mcpslam-mapfusion/macslam/thirdparty/DBoW2/build/CMakeFiles/DBoW2.dir/DUtils/Timestamp.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
