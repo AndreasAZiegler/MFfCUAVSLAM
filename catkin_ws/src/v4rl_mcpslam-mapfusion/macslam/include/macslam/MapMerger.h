060eaaed3d830e816ffcada70074aa28e4771c86
Andreas Ziegler <062.127@gmail.com>
2016-10-26 17:50:48 +0200

    SOFT: Local optimization with loop fusion.

--- catkin_ws/src/v4rl_mcpslam-mapfusion/macslam/include/macslam/MapMerger.h
+++ catkin_ws/src/v4rl_mcpslam-mapfusion/macslam/include/macslam/MapMerger.h
@@ -55,6 +55,7 @@ public:
     MapMerger(MapMergerParams Params, matchptr pMatcher, ros::NodeHandle Nh, ros::NodeHandle NhPrivate);
     mapptr MergeMaps(mapptr pMapCurr, mapptr pMapMatch, MapMatchHit vMatchHit, std::shared_ptr<g2o::Sim3> g2oScw_end);
     void optimizeEssentialGraph(mapptr pFusedMap, mapptr pMapCurr, mapptr pMapMatch, vector<MapMatchHit> vMatchHits);
+    void localMatchOptimization(mapptr mpMap, vector<MapMatchHit> vMatchHits);
     void globalBundleAdjustment(mapptr pFusedMap, mapptr pMapCurr, mapptr pMapMatch, vector<MapMatchHit> vMatchHits, std::shared_ptr<g2o::Sim3> g2oScw);
 
     bool isBusy();
